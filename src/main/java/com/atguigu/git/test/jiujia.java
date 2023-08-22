d8f7b7bab010d60bdc73919914d325e4596e4df4
colaf <colaf@atguigu.com>
2023-08-22 16:36:41 +0800

    crispy脆乐团

---
+++ src/main/java/com/atguigu/git/test/jiujia.java
@@ -0,0 +1,17 @@
+package com.atguigu.git.test;
+
+/**
+ * @BelongsProject: jellyfish-nasal-mucus-vesicle
+ * @BelongsPackage: com.atguigu.git.test
+ * @Author: cola
+ * @CreateTime: 2023-08-22  16:26
+ * @Description: TODO
+ * @Version: 1.0
+ */
+
+public class jiujia {
+    public static void main(String[] args) {
+
+        System.out.println("在旧家把回忆收拾干净");
+    }
+}
