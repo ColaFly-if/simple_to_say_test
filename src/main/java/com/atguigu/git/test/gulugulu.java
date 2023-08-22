df677b8891fa846b0014802ab48a4c510e267bcb
colaf <colaf@atguigu.com>
2023-08-22 15:42:15 +0800

    普信主义

---
+++ src/main/java/com/atguigu/git/test/gulugulu.java
@@ -0,0 +1,16 @@
+package com.atguigu.git.test;
+
+/**
+ * @BelongsProject: git_test
+ * @BelongsPackage: com.atguigu.git.test
+ * @Author: cola
+ * @CreateTime: 2023-08-22  15:36
+ * @Description: TODO
+ * @Version: 1.0
+ */
+
+public class gulugulu {
+    public static void main(String[] args) {
+        System.out.println("我爱你就像拖拉机下山叮铃哐当叮铃");
+    }
+}
