3fa366c6212a753c361bb0e2e92a8daa1e08d844
colaf <colaf@atguigu.com>
2023-08-22 16:42:36 +0800

    crispy脆乐团

---
+++ src/main/java/com/atguigu/git/test/the_end_of_dark.java
@@ -0,0 +1,18 @@
+package com.atguigu.git.test;
+
+/**
+ * @BelongsProject: jellyfish-nasal-mucus-vesicle
+ * @BelongsPackage: com.atguigu.git.test
+ * @Author: cola
+ * @CreateTime: 2023-08-22  16:41
+ * @Description: TODO
+ * @Version: 1.0
+ */
+
+public class the_end_of_dark {
+
+    public static void main(String[] args) {
+
+        System.out.println("在宇宙的角落一起走到黑暗的尽头");
+    }
+}
